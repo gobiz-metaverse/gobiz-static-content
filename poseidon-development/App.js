5aa7e9707c394d4bec453a33b5156408d1314cbe
ducmami <maiminhduc@gobiz.vn>
2024-03-20 09:54:04 +0700

    .

--- poseidon-development/App.js
+++ poseidon-development/App.js
@@ -1,17 +1,17 @@
 function App() {
-  return (
-    <div className="container">
-      <img src="https://i.imgur.com/rTFVdgV.jpg" className="panda" />
-      <h1 className="error">502 Bad Gateway</h1>
-      <p className="text">
-        Xin lỗi, có vẻ như máy chủ của chúng tôi đang gặp sự cố.
-        <br />
-        Chúng tôi đang cố gắng khắc phục sự cố này và sẽ sớm đưa trang web trở lại hoạt động.
-        <br />
-        Trong thời gian chờ đợi, bạn có thể thử lại sau một lúc hoặc truy cập trang web của chúng tôi trên các nền tảng khác.
-      </p>
-    </div>
-  );
+    return (
+        <div className="container">
+            <img src="https://i.imgur.com/rTFVdgV.jpg" className="panda" />
+            <h1 className="error">502 Bad Gateway</h1>
+            <p className="text"> Xin lỗi, có vẻ như máy chủ của chúng tôi đang gặp sự cố.
+                <br /> Chúng tôi đang cố gắng khắc phục sự cố này và sẽ sớm đưa trang web trở lại hoạt động.
+                <br /> Trong thời gian chờ đợi, mời bạn thưởng thức ca khúc của bản release lần này
+            </p>
+            <div className="video-responsive">
+                <iframe width="853" height="480" src={`https://www.youtube.com/embed/FREeSubtAQ8`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title="Embedded youtube" />
+            </div>
+        </div>
+    );
 }
 
 const rootElement = document.getElementById("root");
