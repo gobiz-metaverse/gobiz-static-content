14f58e387b3652e969fe2883dc5ce5965ca47887
ducmami <maiminhduc@gobiz.vn>
2024-08-08 07:53:01 +0700

    .

--- poseidon-development/App.js
+++ poseidon-development/App.js
@@ -6,9 +6,10 @@ function App() {
             <p className="text"> Xin lỗi, có vẻ như máy chủ của chúng tôi đang gặp sự cố.
                 <br /> Chúng tôi đang cố gắng khắc phục sự cố này và sẽ sớm đưa trang web trở lại hoạt động.
                 <br /> Trong thời gian chờ đợi, mời bạn thưởng thức ca khúc của bản release lần này
+
             </p>
             <div className="video-responsive">
-                <iframe width="853" height="480" src={`https://www.youtube.com/embed/UCXao7aTDQM`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title="Embedded youtube" />
+                <iframe width="853" height="480" src={`https://www.youtube.com/embed/-yhqOX3bLvQ`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title="Embedded youtube" />
             </div>
         </div>
     );
